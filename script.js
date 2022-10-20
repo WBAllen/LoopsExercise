d25c9662d0f29b3f1e1ac6eaab6715031e79426d
Ben Allen <live.live.live.island@gmail.com>
2022-10-20 08:38:39 -0400

    first run pre-class

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1
+for (i = 1; i <= 7; i++) {
+    console.log(i)
+};
+
+// 2 (NEED TO REVIEW THIS ONE)
+for (i = 5; i <= 25; i++) {
+    console.log(i*4)
+};
+
+// 3a
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"];
+
+// 3b
+for (friends of wizards) {
+    console.log(friends);
+}
+
+// 4a
+let harryPotterMovies = 0
+
+// 4b (NEED TO REVIEW WHILE LOOPS)
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
